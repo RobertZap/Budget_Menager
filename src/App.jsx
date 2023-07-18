83631676edcb4cd9a317a871577535e9aeccc9e7
RobertZap <zapalowicz.robert@gmail.com>
2023-07-18 12:19:27 +0200

    add images to app, directory to categorise leyouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
-    return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;

3885208fed7017dd0ec555da1e1d9a48e939dee6
RobertZap <zapalowicz.robert@gmail.com>
2023-07-18 12:47:32 +0200

    prepared dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Main, { mainLoader } from "./layouts/Main";
+
+import { logoutAction } from "./actions/logout";
+
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
   },
 ]);
 
